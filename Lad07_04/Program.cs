b60a0e8c2248da123392240cd624a425913d0119
BabiPunSociu <Nguyendung281002@gmail.com>
2023-06-07 15:35:24 +0700

    DungNguyen

---
+++ Lad07_04/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lad07_04
+{
+    //định nghĩa lớp custom ngoại lệ chỉ cho nhập số nguyên dương
+    public class InvalidInputNumber : Exception
+    {
+        public InvalidInputNumber():base("Hay nhap 1 so >0") { }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int intCnt;
+            int intNum = 0;
+            Console.Write("Nhap 1 so :");
+            //nhập và tung ngoại lệ
+            try
+            {
+                intNum = Convert.ToInt32(Console.ReadLine());
+                if (intNum <= 0)
+                {
+                    throw new InvalidInputNumber();
+                }
+            }
+            catch (InvalidInputNumber objInvalidInput)
+            {
+                Console.WriteLine(objInvalidInput.Message);
+            }
+            catch (System.FormatException objFormatException)
+            {
+                Console.WriteLine(objFormatException.Message);
+            }
+            finally
+            {
+                if (intNum > 0)
+                {
+                    //in ra dãy số *100
+                    for (intCnt = 1; intCnt <= 10; intCnt++)
+                        Console.WriteLine(intCnt * intNum);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lad_07/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Store; // using namespace Store
+
+namespace Lad_07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tạo mới nhà phân phối cung cấp đầy đủ namespace
+            Supply.Manufacturer dealer = new Supply.Manufacturer();
+            //gán giá trị
+            dealer.Name = "Coca cola";
+            dealer.Email = "cocacolap@gmail.com";
+            dealer.Phone = "(111)791-8074";
+            //in thông tin
+            Console.WriteLine("Dealer information");
+            Console.WriteLine("\t Name: " + dealer.Name);
+            Console.WriteLine("\t Email: " + dealer.Email);
+            Console.WriteLine("\t Phone: " + dealer.Phone);
+            //tạo mới sản phẩm nằm trong namespace Store
+            StoreItem si = new StoreItem();
+            //gán giá trị
+            si.ItemNo = 613508;
+            si.ItemName = "Fanta";
+            si.Price = 80.00M;
+            //in thông tin
+            Console.WriteLine("Store Inventory");
+            Console.Write("\t Item #: ");
+            Console.WriteLine(si.ItemNo);
+            Console.Write("\t Item Name: ");
+            Console.WriteLine(si.ItemName);
+            Console.Write("\t Unit Price: ");
+            Console.WriteLine(si.Price);
+            Console.Read();
+
+        }
+    }
+}
